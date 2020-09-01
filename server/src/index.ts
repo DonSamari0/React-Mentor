c0c6e43aafa0d2ba492aaa18dc1ff0f228152adb
Angel Martinez <angel.codes139@gmail.com>
2020-09-01 13:47:30 -0500

    husky config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import server from './server';
 
-async function main(){
+async function main() {
     await server.listen(process.env.PORT || 5000, () => {
+        // eslint-disable-next-line no-console
         console.log(`Running server - http://localhost:${process.env.PORT}`);
     });
 }
